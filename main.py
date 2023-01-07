b699f91dc978740483f23a697f228d0f56b788c1
wannacry3030 <wannacry.bat@proton.me>
2023-01-07 19:34:30 -0300

    new file:   template/index.html

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 #essa variavel recebe o parametro flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world"
+    nome = request.args.get("nome")
+    return render_template("index.html", nome=nome)
 
 app.run(debug=True)
