840bd944565e95e3ded7ec1cf0f0fa43adea2857
wannacry3030 <wannacry.bat@proton.me>
2023-01-07 19:54:46 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 #essa variavel recebe o parametro flask
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.sqlite3"
+db = SQLAlchemy(app)
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String())
+    content = db.Column(db.String())
+    author = db.Column(db.String())
+
 
 @app.route("/")
 def home():
+    with app.app_context():
+        db.create_all()
     nome = request.args.get("nome")
     return render_template("index.html", nome=nome)
 
