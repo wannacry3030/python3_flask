842f4e3de922438fbc6e08dc586232086e445711
wannacry3030 <wannacry.bat@proton.me>
2023-01-07 21:02:17 -0300

    sql funcionando nessa build 	modified:   main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 #essa variavel recebe o parametro flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder="template")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.sqlite3"
 db = SQLAlchemy(app)
-
 
 
 class Post(db.Model):
@@ -19,7 +18,18 @@ class Post(db.Model):
 def home():
     with app.app_context():
         db.create_all()
-    nome = request.args.get("nome")
-    return render_template("index.html", nome=nome)
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
 
+@app.route("/post/add", methods=["POST"])
+def add_post():
+    try:
+        form = request.form
+        post = Post(title=form["title"], content=form["content"], author=form["author"])
+        db.session.add(post)
+        db.session.commit()
+    except Exception as error:
+        print("Error", error)
+       
+    
 app.run(debug=True)
